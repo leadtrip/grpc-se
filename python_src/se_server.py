f53f34c9163d6ece539b3308fc0af30758c4e44c
mikewood <mike.wood@secretescapes.com>
2025-02-08 18:03:13 +0000

    Adds client streaming example

--- python_src/se_server.py
+++ python_src/se_server.py
@@ -1,5 +1,5 @@
 from concurrent import futures
-
+from time import sleep
 import grpc
 import SE_pb2
 import SE_pb2_grpc
@@ -8,22 +8,33 @@ from se_client import GRPC_PORT
 
 class SeServer(SE_pb2_grpc.SecretEscapesServicer):
     def GetSeSale(self, request, context):
-        print("getSeSale called")
-        print(request)
+        print(f"GetSeSale called {request}")
         se_sale_reply = SE_pb2.SeSaleReply()
         se_sale_reply.id = request.id
         se_sale_reply.url_slug = 'nice.place.near.the.beach'
         return se_sale_reply
 
     def GetAllSeSales(self, request, context):
-        print("getAllSeSale called")
-        print(request)
+        print(f"GetAllSeSales called {request}")
 
         for i in range(3):
             se_sale_reply = SE_pb2.SeSaleReply()
             se_sale_reply.id = "A"+str(i)
             se_sale_reply.url_slug = 'nice.place.near.the.beach'+str(i)
             yield se_sale_reply
+            sleep(1) # simulate a delay fetching from somewhere
+
+    def GetBatchedSeSales(self, request_iterator, context):
+        print(f"GetBatchedSeSales called")
+        batch_reply = SE_pb2.BatchSeSaleReply()
+        for request in request_iterator:
+            print(request)
+            se_sale_reply = SE_pb2.SeSaleReply()
+            se_sale_reply.id = request.id
+            batch_reply.replies.append(se_sale_reply)
+            sleep(0.5) # simulate a delay fetching from somewhere
+
+        return batch_reply
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- src/main/java/wood/mike/client/SeClient.java
+++ src/main/java/wood/mike/client/SeClient.java
@@ -2,13 +2,14 @@ package wood.mike.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import wood.mike.SeSaleRangeRequest;
-import wood.mike.SeSaleReply;
-import wood.mike.SeSaleRequest;
-import wood.mike.SecretEscapesGrpc;
+import io.grpc.stub.StreamObserver;
+import wood.mike.*;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class SeClient {
@@ -17,6 +18,7 @@ public class SeClient {
     private static final int PORT = 50052;
 
     private final SecretEscapesGrpc.SecretEscapesBlockingStub blockingStub;
+    private final SecretEscapesGrpc.SecretEscapesStub nonBlockingStub;
     private final ManagedChannel channel;
     private final Scanner scanner;
 
@@ -26,13 +28,15 @@ public class SeClient {
                 .build();
 
         blockingStub = SecretEscapesGrpc.newBlockingStub(channel);
+        nonBlockingStub = SecretEscapesGrpc.newStub(channel);
         scanner = new Scanner(System.in);
     }
 
-    private void run() {
+    private void run() throws InterruptedException {
         String result = switch (getChoice()) {
             case 1 -> unaryExample();
             case 2 -> serverStreamingExample();
+            case 3 -> clientStreamingExample();
             default -> throw new IllegalStateException("Unexpected value");
         };
 
@@ -53,10 +57,46 @@ public class SeClient {
                         .setEnd("2025-02-07")
                         .build());
 
-        Stream.generate(() -> null)
-                .takeWhile(x -> seSaleReplyIterator.hasNext())
-                .map(n -> seSaleReplyIterator.next())
-                .forEach(System.out::println);
+        seSaleReplyIterator.forEachRemaining(System.out::println);
+        return "Done";
+    }
+
+    private String clientStreamingExample() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<BatchSeSaleReply> responseObserver = new StreamObserver<>() {
+            @Override
+            public void onNext(BatchSeSaleReply batchSeSaleReply) {
+                batchSeSaleReply.getRepliesList().forEach(req ->
+                        System.out.println("SE sale: " + req.getId())
+                );
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println("Error from server: " + t.getMessage());
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server finished processing.");
+                latch.countDown();
+            }
+        };
+
+        StreamObserver<SeSaleRequest> requestObserver =
+                nonBlockingStub.getBatchedSeSales(responseObserver);
+
+        Stream.of(1,2,3,4,5).map(id ->
+            SeSaleRequest.newBuilder()
+                .setId("A"+id)
+                .build()
+        ).forEach(requestObserver::onNext);
+
+        requestObserver.onCompleted();
+
+        latch.await(5, TimeUnit.SECONDS);
 
         return "Done";
     }
@@ -68,7 +108,7 @@ public class SeClient {
 
 
     private int getChoice() {
-        System.out.println("1. Fetch one SE sale - Urany\n2. Fetch multiple SE sales - server streaming");
+        System.out.println("1. Fetch one SE sale - Urany\n2. Fetch multiple SE sales - server streaming\n3. Fetch multiple SE sales batched - client streaming");
         return scanner.nextInt();
     }
 
